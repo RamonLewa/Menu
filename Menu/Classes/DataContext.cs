e49df0cbd1039a9fc28ab3c94d1c8c69b0a11ff4
RamonLewa <ramonloewenstein@gmail.com>
2024-06-11 23:11:58 -0300

    Início conexão bd

---
+++ Menu/Classes/DataContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiscal;
+using System.Windows.Media.Media3D;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menu.Classes
+{
+    public class ConnectionParams
+    {
+        public static string ConnectionString(string configSoftMaster)
+        {
+            string caminhoBancoDados = "C:\\SGBR\\Master\\ConfigSoftMaster.ini";
+
+            if (File.Exists(configSoftMaster))
+            {
+                string[] linhas = File.ReadAllLines(configSoftMaster);
+                foreach (string linha in linhas)
+                {
+                    if (linha.StartsWith("Conexao="))
+                    {
+                        caminhoBancoDados = linha.Substring("Conexao=".Length);
+                        break;
+                    }
+                }
+
+                if (string.IsNullOrEmpty(caminhoBancoDados))
+                {
+                    throw new Exception("Banco de dados não encontrado");
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("Arquivo ConfigSoftMaster não encontrado", configSoftMaster);
+            }
+
+            return $"DataSource=localhost;Database={caminhoBancoDados};Port=3050;User=SYSDBA;Password=masterkey;Charset=UTF8;Dialect=3;Connection lifetime=15;PacketSize=8192;ServerType=0;Unicode=True;Max Pool Size=1000";
+        }
+    }
+
+    public class DataContext : DbContext
+    {
+        public DbSet<TCliente> TCliente { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            new TClienteTypeConfiguration().Configure(modelBuilder.Entity<TCliente>());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            string configSoftMaster = "C:\\SGBR\\Master\\ConfigSoftMaster.ini";
+            optionsBuilder.UseFirebird(ConnectionParams.ConnectionString(configSoftMaster));
+        }
+    }
+}
---
+++ Menu/Forms/FMenu.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Menu
+{
+    partial class FMenu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 146);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clientes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "FMenu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
